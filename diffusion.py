12a8fe96c93cbdad14c5016ee5d24e3094192c20
lazyCodes7 <rishabmudliar@gmail.com>
2023-01-03 14:07:06 +0530

    Fix: Shape errors in sampling and training

--- diffusion.py
+++ diffusion.py
@@ -1,38 +1,45 @@
+import torch
+import torch.nn as nn
 class Diffusion(nn.Module):
     def __init__(
         self,
-        beta_start,
-        beta_end,
+        device,
+        beta_start=1e-4, 
+        beta_end=0.02,
+        img_size = 32,
         timesteps = 1000
     ):
+        self.beta_start = beta_start
+        self.beta_end = beta_end
         self.timesteps = timesteps
+        self.device = device
         self.noise_scheduler = self.init_noise_scheduler()
         self.alpha = 1 - self.noise_scheduler
-        self.alpha_bar = torch.cumprod(self.noise_scheduler)
+        self.alpha_bar = torch.cumprod(self.noise_scheduler, dim = 0)
+
+        self.img_size = img_size
 
     def sample_timesteps(self, batch_size):
-        t = 0
-        while(t>1):
-            t = torch.randint(0, self.timesteps, size = (batch_size, ))
+        t = torch.randint(0, self.timesteps, size = (batch_size, ))
         return t
     
     def init_noise_scheduler(self):
-        return torch.linspace(beta_start, beta_end, self.timesteps)
+        return torch.linspace(self.beta_start, self.beta_end, self.timesteps)
     
     def sample_noise_image(self, image, timestep):
-        sqrt_alpha_bar = torch.sqrt(self.alpha[t])
-        sqrt_one_minus_alpha_bar = torch.sqrt(1 - self.alpha_bar[t])
+        sqrt_alpha_bar = torch.sqrt(self.alpha_bar[timestep])[:, None, None, None]
+        sqrt_one_minus_alpha_bar = torch.sqrt(1 - self.alpha_bar[timestep])[:, None, None, None]
         eps = torch.randn_like(image)
         noise_image = sqrt_alpha_bar*image + sqrt_one_minus_alpha_bar*eps
-        return noise_image
+        return noise_image, eps
     
-    def sample_initial_image(self, model, timestep):
+    def sample_initial_image(self, model, batch_size):
         model.eval()
-        x = torch.randn((n, 3, self.img_size, self.img_size)).to(self.device)
-        for t in range(timestep, -1, -1):
-            alpha_bar = self.alpha[t]
+        x = torch.randn((batch_size, 3, self.img_size, self.img_size)).to(self.device)
+        for t in range(self.timesteps, -1, -1):
+            alpha_bar = self.alpha[t][:, None, None, None]
             predicted_noise = model(x, t)
-            sigma = self.noise_scheduler[t]
+            sigma = self.noise_scheduler[t][:, None, None, None]
             if t > 1:
                 noise = torch.randn_like(x)
             else:
