715be361c8c79ed2c241d78ce0023cc6c971fee3
lazyCodes7 <rishabmudliar@gmail.com>
2023-01-04 10:12:04 +0530

    Fix: GPU memory issues while sampling

--- diffusion.py
+++ diffusion.py
@@ -34,20 +34,20 @@ class Diffusion(nn.Module):
         return noise_image, eps
     
     def sample_initial_image(self, model, batch_size):
-        model.eval()
-        x = torch.randn((batch_size, 3, self.img_size, self.img_size)).to(self.device)
-        for timestep in range(self.timesteps-1, -1, -1):
-            t = (torch.ones(batch_size) * timestep).long().to(self.device)
-            #print(self.alpha.shape)
-            alpha = self.alpha[t][:, None, None, None]
-            alpha_bar = self.alpha_bar[t][:, None, None, None]
-            predicted_noise = model(x, t)
-            sigma = self.noise_scheduler[t][:, None, None, None]
-            if timestep > 1:
-                noise = torch.randn_like(x).to(self.device)
-            else:
-                noise = torch.zeros_like(x).to(self.device)
-            x = 1 / torch.sqrt(alpha) * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_bar))) * predicted_noise) + torch.sqrt(sigma) * noise
+        with torch.no_grad():
+          model.eval()
+          x = torch.randn((batch_size, 3, self.img_size, self.img_size)).to(self.device)
+          for timestep in range(self.timesteps-1, -1, -1):
+              t = (torch.ones(batch_size) * timestep).long().to(self.device)
+              alpha = self.alpha[t][:, None, None, None]
+              alpha_bar = self.alpha_bar[t][:, None, None, None]
+              predicted_noise = model(x, t)
+              sigma = self.noise_scheduler[t][:, None, None, None]
+              if timestep > 1:
+                  noise = torch.randn_like(x)
+              else:
+                  noise = torch.zeros_like(x)
+              x = 1 / torch.sqrt(alpha) * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_bar))) * predicted_noise) + torch.sqrt(sigma) * noise
         model.train()
         x = (x.clamp(-1, 1) + 1) / 2
         x = (x * 255).type(torch.uint8)
